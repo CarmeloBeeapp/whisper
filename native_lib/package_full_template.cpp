7c2841d1e34ee9505c372093f1dba11dc9e27e94
Your Name <you@example.com>
2024-04-18 20:35:07 +0700

    test compile whisper.cpp latest work

--- native_lib/package_full_template.cpp
+++ native_lib/package_full_template.cpp
@@ -10,44 +10,37 @@
 #include "lib/json/json.hpp"
 using json = nlohmann::json;
 
-char *jsonToChar(json jsonData)
-{
+char* jsonToChar(json jsonData) {
     std::string result = jsonData.dump();
-    char *ch = new char[result.size() + 1];
+    char* ch = new char[result.size() + 1];
     strcpy(ch, result.c_str());
     return ch;
 }
 
-std::string charToString(char *value)
-{
+std::string charToString(char* value) {
     std::string result(value);
     return result;
 }
 
-char *stringToChar(std::string value)
-{
-    char *ch = new char[value.size() + 1];
+char* stringToChar(std::string value) {
+    char* ch = new char[value.size() + 1];
     strcpy(ch, value.c_str());
     return ch;
 }
 
 extern "C"
 {
-    void print(char *value)
-    {
+    void print(char* value) {
         std::cout << charToString(value) << std::endl;
     }
-    int calculate(int num_1, int num_2)
-    {
+    int calculate(int num_1, int num_2) {
         return num_1 * num_2;
     }
-    char *request(char *data)
-    {
+    char* request(char* data) {
         json jsonBody = json::parse(data);
         return jsonToChar(jsonBody);
     }
-    int main()
-    {
+    int main() {
         print(stringToChar("oke"));
         return 0;
     }
